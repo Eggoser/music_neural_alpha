cffa0648b78f9f7417d032ffd24722985d41b867
Eggoser <titanicsv@yandex.ru>
2021-05-04 04:08:42 +0300

    all dente

--- manage.py
+++ manage.py
@@ -1,22 +1,11 @@
-# from app import create_app
+from app import create_app
 
-# app = create_app()
+app = create_app()
 
-from flask import Flask
-app = Flask(__name__)
 
-app.config["APPLICATION_ROOT"] = "/music-neural"
-
-@app.route("/")
-def hello():
-	return "hello"
-
-# @app.errorhandler(404)
-# def error_404(e):
-# 	# print(dir(e))
-# 	# print(e)
-# 	# return "<h2>Ошибка 404, перейдите в корень сайта, должно помочь)</h2>"
-# 	return str(e.get_response())
+@app.errorhandler(404)
+def error_404(e):
+	return "<h2>Ошибка 404, перейдите в корень сайта, должно помочь)</h2>"
 
 @app.errorhandler(500)
 def error_500(e):
